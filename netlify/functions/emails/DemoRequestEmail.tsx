e687ba4a88261bc097107535603fc04e50568245
Guy Tzaban <guy.tzaban@centrica.com>
2025-02-23 20:57:23 +0200

    Enhance DemoRequestEmail template with improved styling and layout
    - Add base URL for logo and static assets
    - Implement responsive design with container and section styling
    - Include logo, preview text, and decorative border elements
    - Improve overall visual structure of the email template

--- netlify/functions/emails/DemoRequestEmail.tsx
+++ netlify/functions/emails/DemoRequestEmail.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 import {
-  Html,
-  Head,
   Body,
+  Column,
   Container,
-  Section,
+  Head,
   Heading,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
   Text,
 } from "@react-email/components";
 
@@ -16,6 +21,8 @@ interface DemoRequestProps {
   account: string;
   partner: string;
 }
+
+const baseUrl = 'https://optimize.powerradar.energy';
 
 const DemoRequestEmail: React.FC<DemoRequestProps> = ({
   fullName,
@@ -28,8 +35,19 @@ const DemoRequestEmail: React.FC<DemoRequestProps> = ({
     <Html>
       <Head />
       <Body style={main}>
-        <Container>
-          <Section>
+        <Preview>New Demo Request</Preview>
+        <Container style={container}>
+          <Section style={logo}>
+            <Img width={114} src={`${baseUrl}/static/logo.png`} />
+          </Section>
+          <Section style={sectionsBorders}>
+            <Row>
+              <Column style={sectionBorder} />
+              <Column style={sectionCenter} />
+              <Column style={sectionBorder} />
+            </Row>
+          </Section>
+          <Section  style={content}>
             <Heading style={heading}>New Demo Request</Heading>
             <Text style={text}>
               <strong>Full Name:</strong> {fullName}
@@ -57,6 +75,38 @@ const main = {
   backgroundColor: "#ffffff",
   fontFamily:
     '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  maxWidth: '580px',
+  margin: '30px auto',
+  backgroundColor: '#ffffff',
+};
+
+const content = {
+  padding: '5px 20px 10px 20px',
+};
+
+const logo = {
+  display: 'flex',
+  justifyContent: 'center',
+  alingItems: 'center',
+  padding: 30,
+};
+
+const sectionsBorders = {
+  width: '100%',
+  display: 'flex',
+};
+
+const sectionBorder = {
+  borderBottom: '1px solid rgb(238,238,238)',
+  width: '249px',
+};
+
+const sectionCenter = {
+  borderBottom: '1px solid #ff6f0f',
+  width: '102px',
 };
 
 const heading = {
