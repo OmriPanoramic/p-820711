ac5bbe74b0cf754073f789d6b3e887093d3c105c
Guy Tzaban <guy.tzaban@centrica.com>
2025-03-10 15:25:54 +0200

    Update Section4 header text and imagery for Panoramic Optimize

--- src/components/section4.tsx
+++ src/components/section4.tsx
@@ -22,8 +22,8 @@ export function Section4() {
   return (
     <div className="mb-20 flex w-full flex-1 shrink-0 flex-col gap-4 self-stretch">
       <div className="mb-6 text-center lg:mb-12">
-        <h3 className="mb-2 text-sm text-primary">Success stories</h3>
-        <h2 className="mb-4 text-h2">Users love Panoramic</h2>
+        <h3 className="mb-2 text-sm text-primary">The Panoramic Optimize</h3>
+        <h2 className="mb-4 text-h2">Panoramic Optimize: Premium insights,<br />peak performance. See how.</h2>
       </div>
       <AspectRatio ratio={16 / 9}>
         <div className="ac flex h-full w-full items-center justify-center overflow-hidden rounded-lg bg-[#D5D5D5]">
