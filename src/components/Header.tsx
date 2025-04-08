e36819b8a04fdb16f07bb3209ed5a2ed387490d3
Guy Tzaban <guy.tzaban@centrica.com>
2025-04-08 20:03:14 +0300

    Refactor Header component for improved readability and mobile menu functionality
    - Clean up code formatting and spacing for better readability
    - Update button labels for clarity
    - Enhance mobile menu structure and styling
    - Ensure consistent layout and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,14 @@ import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { DemoModal } from "@/components/DemoModal";
 import { UpgradeModal } from "@/components/UpgradeModal";
-import logo from '@/assets/logo-header.svg'
+import logo from "@/assets/logo-header.svg";
 import { AnimatePresence, motion } from "motion/react";
 
-export const Header = ({ scrollRef }: { scrollRef: React.RefObject<HTMLDivElement> }) => {
+export const Header = ({
+  scrollRef,
+}: {
+  scrollRef: React.RefObject<HTMLDivElement>;
+}) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isDemoModalOpen, setIsDemoModalOpen] = useState(false);
   const [isUpgradeModalOpen, setIsUpgradeModalOpen] = useState(false);
@@ -15,15 +19,16 @@ export const Header = ({ scrollRef }: { scrollRef: React.RefObject<HTMLDivElemen
     const element = document.getElementById(sectionId);
     if (element && scrollRef.current) {
       const headerOffset = 110;
-      
+
       // Get the element's position relative to the scrollable container
       const containerRect = scrollRef.current.getBoundingClientRect();
       const elementRect = element.getBoundingClientRect();
       const relativePosition = elementRect.top - containerRect.top;
-      
+
       // Calculate the final scroll position
-      const scrollPosition = scrollRef.current.scrollTop + relativePosition - headerOffset;
-      
+      const scrollPosition =
+        scrollRef.current.scrollTop + relativePosition - headerOffset;
+
       // Perform the scroll
       scrollRef.current.scrollTo({
         top: scrollPosition,
@@ -45,19 +50,21 @@ export const Header = ({ scrollRef }: { scrollRef: React.RefObject<HTMLDivElemen
 
   return (
     <>
-      <header className="sticky top-0 z-50 border-b border-border/40 bg-background/50 backdrop-blur supports-[backdrop-filter]:bg-background/50 w-full md:w-[calc(100%-12px)]">
+      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/50 backdrop-blur supports-[backdrop-filter]:bg-background/50 md:w-[calc(100%-12px)]">
         <div className="container flex h-16 items-center justify-between">
           {/* Logo */}
           <div className="flex items-center gap-2">
             <button
               data-fs-track="logo-click"
               onClick={() => scrollToSection("section1")}
-              className="flex items-center space-x-2 gap-2"
+              className="flex items-center gap-2 space-x-2"
             >
-              <img src={logo} alt="logo"  />
-              <div className="w-[1px] h-[28px] bg-[#EBEBEB]" />
-              <div className="text-[16px] font-bold leading-[16px] text-primary text-start">
-                Panoramic<br />Power
+              <img src={logo} alt="logo" />
+              <div className="h-[28px] w-[1px] bg-[#EBEBEB]" />
+              <div className="text-start text-[16px] font-bold leading-[16px] text-primary">
+                Panoramic
+                <br />
+                Power
               </div>
             </button>
           </div>
@@ -91,19 +98,19 @@ export const Header = ({ scrollRef }: { scrollRef: React.RefObject<HTMLDivElemen
             >
               Effortless Migration
             </button>
-            {/* <button
+            <button
               data-fs-track="testimonials-button"
               onClick={() => scrollToSection("section4")}
               className="text-sm text-muted-foreground transition-colors hover:text-primary"
             >
-              Testimonials
-            </button> */}
+              Video
+            </button>
             <button
               data-fs-track="optimize-experience-button"
               onClick={() => scrollToSection("section5")}
               className="text-sm text-muted-foreground transition-colors hover:text-primary"
             >
-              Optimize Experience
+              The Optimize Experience
             </button>
             <button
               data-fs-track="pricing-button"
@@ -138,70 +145,80 @@ export const Header = ({ scrollRef }: { scrollRef: React.RefObject<HTMLDivElemen
 
         {/* Mobile Menu */}
         <AnimatePresence>
-        {isMobileMenuOpen && (
-          <motion.div 
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: "auto" }}
-            exit={{ opacity: 0, height: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            <nav className="flex flex-col space-y-4 border-b bg-background px-4 py-6 shadow-lg border-t border-border/40">
-              <button
-                data-fs-track="whats-new-button"
-                onClick={() => scrollToSection("section2")}
-                className="text-sm text-muted-foreground transition-colors hover:text-primary"
-              >
-                What's New?
-              </button>
-              <button
-                data-fs-track="effortless-migration-button"
-                onClick={() => scrollToSection("section3")}
-                className="text-sm text-muted-foreground transition-colors hover:text-primary"
-              >
-                Effortless Migration
-              </button>
-             {/*  <button
-                onClick={() => scrollToSection("section4")}
-                className="text-sm text-muted-foreground transition-colors hover:text-primary"
-              >
-                Testimonials
-              </button> */}
-              <button
-                data-fs-track="optimize-experience-button"
-                onClick={() => scrollToSection("section5")}
-                className="text-sm text-muted-foreground transition-colors hover:text-primary"
-              >
-                Optimize Experience
-              </button>
-              <button
-                data-fs-track="pricing-button"
-                onClick={() => scrollToSection("section6")}
-                className="text-sm text-muted-foreground transition-colors hover:text-primary"
-              >
-                Pricing
-              </button>
-              <button
-                data-fs-track="contact-button"
-                onClick={() => scrollToSection("footer")}
-                className="text-sm text-muted-foreground transition-colors hover:text-primary"
-              >
-                Contact
-              </button>
-              <div className="flex flex-col space-y-4 pt-4">
-                <Button variant="ghost" asChild className="w-full" data-fs-track="book-demo-button">
-                  <a onClick={handleDemoClick} href="#">
-                    Book a Demo
-                  </a>
-                </Button>
-                <Button asChild className="w-full" data-fs-track="upgrade-now-button">
-                  <a onClick={handleUpgradeClick} href="#">
-                    Upgrade Now
-                  </a>
-                </Button>
-              </div>
-            </nav>
-          </motion.div>
-        )}</AnimatePresence>
+          {isMobileMenuOpen && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              exit={{ opacity: 0, height: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              <nav className="flex flex-col space-y-4 border-b border-t border-border/40 bg-background px-4 py-6 shadow-lg">
+                <button
+                  data-fs-track="whats-new-button"
+                  onClick={() => scrollToSection("section2")}
+                  className="text-sm text-muted-foreground transition-colors hover:text-primary"
+                >
+                  What's New?
+                </button>
+                <button
+                  data-fs-track="effortless-migration-button"
+                  onClick={() => scrollToSection("section3")}
+                  className="text-sm text-muted-foreground transition-colors hover:text-primary"
+                >
+                  Effortless Migration
+                </button>
+                <button
+                  onClick={() => scrollToSection("section4")}
+                  className="text-sm text-muted-foreground transition-colors hover:text-primary"
+                >
+                  Video
+                </button>
+                <button
+                  data-fs-track="optimize-experience-button"
+                  onClick={() => scrollToSection("section5")}
+                  className="text-sm text-muted-foreground transition-colors hover:text-primary"
+                >
+                  The Optimize Experience
+                </button>
+                <button
+                  data-fs-track="pricing-button"
+                  onClick={() => scrollToSection("section6")}
+                  className="text-sm text-muted-foreground transition-colors hover:text-primary"
+                >
+                  Pricing
+                </button>
+                <button
+                  data-fs-track="contact-button"
+                  onClick={() => scrollToSection("footer")}
+                  className="text-sm text-muted-foreground transition-colors hover:text-primary"
+                >
+                  Contact
+                </button>
+                <div className="flex flex-col space-y-4 pt-4">
+                  <Button
+                    variant="ghost"
+                    asChild
+                    className="w-full"
+                    data-fs-track="book-demo-button"
+                  >
+                    <a onClick={handleDemoClick} href="#">
+                      Book a Demo
+                    </a>
+                  </Button>
+                  <Button
+                    asChild
+                    className="w-full"
+                    data-fs-track="upgrade-now-button"
+                  >
+                    <a onClick={handleUpgradeClick} href="#">
+                      Upgrade Now
+                    </a>
+                  </Button>
+                </div>
+              </nav>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </header>
 
       <DemoModal
