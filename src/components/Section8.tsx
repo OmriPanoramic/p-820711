f602b241856d225d2d13f47b43cbf403f6a456a6
omrilandman <omriland@gmail.com>
2025-04-20 09:28:00 +0300

    Small padding changes

--- src/components/Section8.tsx
+++ src/components/Section8.tsx
@@ -3,7 +3,7 @@ import PricingCalculator from "./PricingCalculator";
 export const Section8 = () => {
   return (
     <div
-      className="flex w-full flex-col items-center pt-32 pb-32"
+      className="flex w-full flex-col items-center pt-32"
       id="PricingCalculator"
     >
       <PricingCalculator />
